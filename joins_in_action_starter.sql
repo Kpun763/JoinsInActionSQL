b526891dac3587402cc5d8ea6aa883fff046ddef
Kyle Washington <kmw1993@msn.com>
2023-08-11 14:31:31 -0400

    Uploading JoinInAction SQL project

--- joins_in_action_starter.sql
+++ joins_in_action_starter.sql
@@ -11,30 +11,39 @@ SELECT * FROM movies RIGHT JOIN users on movie_id = favorite_movie_id;
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 1 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a left join to see all records from the movies table and matching records from the users table.
 -- EXPECTED RESULT: 75 Rows Returned
-
+Select * from movies 
+left join users on movie_id = favorite_movie_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 2 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a right join to see all records from the users table and matching records from the movies table.
 -- EXPECTED RESULT: 16 Rows Returned
-
+select * from users
+right join movies on favorite_movie_id = movie_id
+where favorite_movie_id is not null;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 3 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a inner join to get only the rows from the movies tables that have matching rows in the users tables.
 -- EXPECTED RESULT: 14 Rows Returned
-
+Select * from movies Inner Join users on movie_id = favorite_movie_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 4 >>>>>>>>>>>>>>>>>>>>>>>
 -- Determine which lead studio's movies are favorited by users the most.
 -- EXPECTED RESULT: Disney
-
+select lead_studio from movies 
+left join users on movie_id = favorite_movie_id
+group by lead_studio
+order by count(favorite_movie_id) desc
+limit 1;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 5 >>>>>>>>>>>>>>>>>>>>>>>
 -- Get the average Rotten Tomatoes score of all movies that are favorited by a user.
 -- EXPECTED RESULT: 52.21
-
-
+Select avg(rotten_tomatoes) from movies
+inner join users on movie_id = favorite_movie_id;
+ 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 6 >>>>>>>>>>>>>>>>>>>>>>>
 -- Determine which two movies are have duplicate entries in the dataset. (HINT: No join is needed for this problem)
 -- EXPECTED RESULT: Mamma Mia!, Gnomeo and Juliet
 -- It is important to be able to identify dirty data within a dataset!
-
+Select film from movies
+group by film having count(*) > 1;
